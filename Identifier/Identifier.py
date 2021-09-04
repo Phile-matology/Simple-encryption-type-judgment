e4101c91be8f1658c7fcd55caf200ece5f9c6054
Phile-matology <79824646+Phile-matology@users.noreply.github.com>
2021-09-04 23:20:55 +0800

    Add Base16,32,64,58,85

--- Identifier/Identifier.py
+++ Identifier/Identifier.py
@@ -1,5 +1,15 @@
 import sys
 import pybase100 as base100
+import base64
+import base58
+import re
+
+global Pattern 
+Pattern ={"Base64":"^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$",
+          "Base32":"^([A-Z2-7]{8})*(([A-Z2-7]{8})|([A-Z2-7]{7}=)|([A-Z2-7]{5}===)|([A-Z2-7]{4}====)|([A-Z2-7]{2}======))$",
+          "Base16":"^([A-F0-9]{2})*$",
+          "Base58":"^[A-HJ-NP-Za-km-z1-9]*$",
+          "Base85":"^[!\"#\$%&'\(\)\*\+,-\./0-9:;<=>\?@A-Z\[\\\]\^_`a-u]*$"}
 
 def Client():
     print("Type 'help' or 'h' for more information, 'use'or 'u' to get started, and 'quit' or 'q' to leave. \n>",end="")
@@ -24,14 +34,34 @@ def Identification():
     #Identify some special encryption method.
     if isEmoji(ciphertext[0]):
         Plaintext["Base100"]= ""
-    #Traverse the ciphertext and statistical character characteristics.
-
+    #Matching ciphertext using regular expressions
+    '''
+    在这里我有一个想法，应该可以通过统计密文长度关系排除一部分不应匹配的加密方式。
+    另一个想法是，开一个选项，让用户设置是省略不匹配的直接放弃，还是以replace的方式弄出来，毕竟有可能有密文有点损失？
+    '''
+    global Pattern
+    for Pt in Pattern.keys():
+        if re.match(Pattern[Pt],ciphertext):
+            Plaintext[Pt]=""
     #guess the coding method by using the exclusion method and work out the result.
     if "Base100" in Plaintext:
         Plaintext["Base100"]=base100.decode(ciphertext)
+    if "Base64" in Plaintext:
+        Plaintext["Base64"]=base64.b64decode(ciphertext)
+    if "Base32" in Plaintext:
+        Plaintext["Base32"]=base64.b32decode(ciphertext)
+    if "Base16" in Plaintext:
+        Plaintext["Base16"]=base64.b16decode(ciphertext)
+    if "Base58" in Plaintext:
+        Plaintext["Base58"]=base58.b58decode(ciphertext)
+    if "Base85" in Plaintext:
+        Plaintext["Base85"]=base64.b85decode(ciphertext)
     #Give out possible answers.
     for ans in Plaintext:
-        print("Method:"+ans+"    Plaintext:"+Plaintext[ans].decode('utf-8'))
+        try:
+            print("Method:"+ans+"    Plaintext:"+Plaintext[ans].decode(encoding='utf-8'))
+        except UnicodeDecodeError:
+            print("Method:"+ans+"    Matched but decoding error, this may be a wrong way.")
 
 def isEmoji(content):
     if not content:
@@ -49,6 +79,6 @@ def isEmoji(content):
 
 #Below are the main function.
 print("Welcome to Simple Encryption Type Identifier.\n")
-print("Version: 0.1.0\n")
-print("Functions now include: Base100.\n")
+print("Version: 0.1.1\n")
+print("Functions now include: Base100, Base64,Base16.\n")
 Client()
--- README.md
+++ README.md
@@ -4,7 +4,15 @@ This repository is intended for CTF beginners to  distinguish the encryption met
 
 Also, it imports some third party libraries to support the function of decoder.
 
-The identifier can now decode: Base100.		--09/03/21
+The identifier can now deal with: Base100.		--09/03/21
+
+New Function: Base64, Base32, Base16, Base58.		--09/04/21
+
 ## Credits
 A huge thanks to MasterGroosha for his Base100 coder written in python.
 (https://github.com/MasterGroosha/pybase100/)
+
+Work by mufeedvh also brought me inspiring ideas on my repo.
+
+(https://github.com/mufeedvh/basecrack)
+
